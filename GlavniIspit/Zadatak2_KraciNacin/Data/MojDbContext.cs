663cee703754d2d419e87bdfc35249999f9365b8
Katarina Blažević <katarina.blazevic@iusinfo.hr>
2024-05-02 16:01:51 +0200

    Dodan kraći način rješavanja API zadatka (scaffolding)

---
+++ GlavniIspit/Zadatak2_KraciNacin/Data/MojDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Zadatak2_KraciNacin.Models;
+
+namespace Zadatak2_KraciNacin.Data
+{
+	public class MojDbContext : DbContext
+	{
+		public MojDbContext(DbContextOptions<MojDbContext> options) 
+			: base(options) 
+		{
+			
+		}
+
+		public DbSet<Todo> Todo { get; set; }
+	}
+}
